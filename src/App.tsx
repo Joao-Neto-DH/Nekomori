8131f8a21326653af44ed34355abe1c63477d07e
Joao-Neto-DH <joaolima882018@gmail.com>
2023-03-18 14:06:53 +0100

    Preparando o ambiente

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+// import './input.css';
+
+import Button from "./components/Button";
+import Header, { GlobalStyle } from "./components/Header";
+import Logo from "./components/Logo";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <GlobalStyle />
+      <Header>
+        <>
+          <Logo className="active" gender="primary" as="a" href="/">
+              <span className="ri-home-fill"></span>
+          </Logo>
+
+          <div>
+              <img src="/nekomori-logo.png" width="150" alt="anime nekomori"/>
+
+              <form action="/search" method="get">
+                  <input type="text" name="search" id="search" placeholder="Buscar por animes" required/>
+                  <button type="submit" aria-label="pesquisar" className="ri-search-line"></button>
+              </form>
+
+              <div className="profile">
+                  <button className="ri-alarm-fill" aria-label="agenda"></button>
+                  <button className="ri-notification-2-fill" aria-label="notificações"></button>
+                  <button className="user" title="joão neto" aria-label="perfil">
+                      <div className="user-img">
+                          <img src="/naruto.jpg" alt="joão neto"/>
+                      </div>
+                      <ul>
+                          <li><a href="#">Perfil</a></li>
+                          <li><a href="#">Definições</a></li>
+                          <li><a href="#">Favoritos</a></li>
+                          <li><a href="#">Sair</a></li>
+                      </ul>
+                  </button>
+              </div>
+          </div>
+        </>
+      </Header>
+      <Button gender="primary">Hola</Button>
+      <Button gender="secondary">Hola</Button>
+    </>
   )
 }
 
