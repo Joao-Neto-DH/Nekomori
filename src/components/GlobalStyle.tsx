9ce81f638f6fe5b7cffc12d3875e42e956ee669a
Joao-Neto-DH <joaolima882018@gmail.com>
2023-03-20 17:36:01 +0100

    Estilizando barra de rolagem

--- src/components/GlobalStyle.tsx
+++ src/components/GlobalStyle.tsx
@@ -1,7 +1,15 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-
+    ::-webkit-scrollbar{
+        width: 8px;
+        height: 8px;
+        background-color: transparent;
+    }
+    ::-webkit-scrollbar-thumb{
+        border-radius: 12px;
+        background-color: #171a22;
+    }
     body{
         margin: 0;
         background-color: ${props => props.theme.colors.main};
